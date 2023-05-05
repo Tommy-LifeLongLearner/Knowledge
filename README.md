7540a9909b3f4f20abfd42ba61d0b1b3c0dfbc11
Saadi Toumi Fouad <saaditoumifouad@gmail.com>
2023-05-05 21:21:30 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+### Knowledge
+When learning new things they're kept in the temporary memory, without practicing or revisiting they will be forgotten, Knowledge helps saving that knowledge as articles, so you can refer to what you learnt anytime
+It's a user friendly out of the box solution as a desktop app
+
+### Getting Started
+Clone the project to your PC then inside the project directory run the following commands
+```txt
+npm install
+electron .
+```
+
+### Contact
+- saaditoumifouad@gmail.com
+
+### License
+The MIT License
---
+++ index.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow } = require('electron');
+const gotTheLock = app.requestSingleInstanceLock();
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+  // hides the menu
+  // win.setMenu(null);
+  win.webContents.toggleDevTools();
+}
+
+app.on('window-all-closed', () => {
+  if(process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+if(!gotTheLock) {
+  app.quit();
+}else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if(win) {
+      if(win.isMinimized()) {
+        win.restore();
+      }
+      win.focus();
+    }
+  })
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  })
+})
